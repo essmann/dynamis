bb59032f112d35a1278e3d2c3040e19567dac0e3
essmann <kenwilliam1000@gmail.com>
2026-09-05 09:56:15 +0200

    project: added package.json workspaces. npm packages will be handled at root now

---
+++ packages/shared-styles/src/index.ts
@@ -0,0 +1,8 @@
+export const sharedStyles = {
+    primary: "#008CFF",
+    bg: "#FAFAFA",
+    bgHover: "#F0F0F0",
+    bgSurface: "#FFFFFF",
+    bgSurfaceHover: "#F5F5F5",
+    text: "#000000"
+}
